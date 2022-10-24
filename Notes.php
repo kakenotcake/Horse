99a7efe92f08f26d048f68a94449af02a89a0a4a
kakenotcake <76403436+kakenotcake@users.noreply.github.com>
2022-10-24 10:13:13 -0400

    Merge origin/master
    Conflicts:
    	domain/ApplicantScreening.php

---
+++ Notes.php
@@ -0,0 +1,38 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class Notes {
+    private $horseName; //string
+    private $date; //date format
+    private $note; //string
+    private $trainerFirstName; //string
+    private $trainerLastName; //string
+    
+    function __construct($horseName, $date, $note, $trainerFirstName, $trainerLastName) {
+            $this->horseName = $horseName;
+            $this->date = $date;
+            $this->note = $note;
+            $this->trainerFirstName = $trainerFirstName;
+            $this->trainerLastName = $trainerLastName;
+    }
+    function get_horseName() {
+        return $this->horseName;
+    }
+    function get_date() {
+        return $this->date;
+    }
+    function get_note() {
+        return $this->note;
+    }
+    function get_trainerFirstName() {
+        return $this->trainerFirstName;
+    }
+    function get_trainerLastName() {
+        return $this->trainerLastName;
+    }
+}
