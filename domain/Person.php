3eac80015be0d9e483f579e39d4b60937240d7fe
kakenotcake <76403436+kakenotcake@users.noreply.github.com>
2022-10-24 13:35:09 -0400

    Merge origin/master

--- domain/Person.php
+++ domain/Person.php
@@ -1,4 +1,36 @@
 <?php
+
+class Person {
+    private $firstName; //string
+    private $lastName; //string
+    private $email; //string
+    private $phone; //int
+    private $access; //int
+    
+    function __construct($firstName, $lastName, $email, $phone, $access) {
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->email = $email;
+        $this->phone = $phone;
+        $this->access = $access;
+    }
+    function get_firstName() {
+        return $this->firstName;
+    }
+    function get_lastName() {
+        return $this->lastName;
+    }
+    function get_email() {
+        return $this->email;
+    }
+    function get_phone() {
+        return $this->phone;
+    }
+
+    function get_access() {
+        return $this->access;
+    }
+}
 /*WILL NOT BE USING CLASS!!
 
  * Copyright 2013 by Allen Tucker. 
