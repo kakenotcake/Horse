26b637522bbc166bd7328357102f35393aae32b3
Andrei Catuneanu Cica <1434707@gmail.com>
2022-11-07 12:51:55 -0500

    Trainer file has all of the trainer information and getter funcctions

--- domain/Trainer.php
+++ domain/Trainer.php
@@ -1,41 +1,42 @@
 <?php
 
-/* 
+/*
 *comment test
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  trainer
  */
+//First name, last name, phone number, user name, and password
 
 class Trainer {
-    private $trainerName; //string
-    private $color; //string
-    private $breed; //string - can be null if not known
-    private $pastureNum; //int
-    private $colorRank; //string
-    
-    function __construct($trainerName, $color, $breed, $pastureNum, $colorRank) {
-        $this->trainerName = $name;
-        $this->color = $color;
-        $this->breed = $breed;
-        $this->pastureNum = $pastureNum;
-        $this->colorRank = $colorRank;
+    private $trainerFirstName; //string
+    private $trainerLastName; //string
+    private $phoneNumber; //string
+    private $username; //string
+    private $password; //string
+
+    function __construct($trainerFirstName, $phoneNumber, $username, $password, $trainerLastName) {
+        $this->trainerFirstName = $name;
+        $this->phoneNumber = $phoneNumber;
+        $this->username = $username;
+        $this->password = $password;
+        $this->trainerLastName = $trainerLastName;
     }
-    function get_trainerName() {
-        return $this->trainerName;
+    function get_trainerFirstName() {
+        return $this->trainerFirstName;
     }
-    function get_color() {
-        return $this->color;
+    function get_trainerLastName() {
+        return $this->trainerLastName;
     }
-    function get_breed() {
-        return $this->breed;
+    function get_phoneNumber() {
+        return $this->phoneNumber;
     }
-    function get_pastureNum() {
-        return $this->pastureNum;
+    function get_username() {
+        return $this->username;
     }
-    function get_colorRank() {
-        return $this->colorRank;
+    function get_password() {
+        return $this->password;
     }
 }
 
