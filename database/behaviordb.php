563fa2955b95f9e26cb949967c9601c0e3347225
kakenotcake <76403436+kakenotcake@users.noreply.github.com>
2022-11-16 11:30:38 -0500

    Merge origin/master

--- database/behaviordb.php
+++ database/behaviordb.php
@@ -6,17 +6,17 @@
  * and open the template in the editor.
  */
 include_once('dbinfo.php');
-include_once(dirname(__FILE__).'/../domain.Behavior.php');
+include_once(dirname(__FILE__).'/../domain/Behavior.php');
 /*
  * Add behavior to behavior table: if already there, return false
  */
 function add_behavior($behavior) {
-    if (!behavior instanceof Behvaior) {
+    if (!$behavior instanceof Behavior) {
         die("Error: add_behavior type mismatch");
     }
     $con=connect();
     $query = "SELECT * FROM behaviordb WHERE title='" . $behavior->get_title() . "';";
-    $result = mysqli_querty($con,$query);
+    $result = mysqli_query($con,$query);
     
     //if there is no entry for that title, then add it
     if ($result == null || mysqli_num_rows($result) == 0) {
@@ -29,5 +29,24 @@ function add_behavior($behavior) {
     mysqli_close($con);
     return false;
     
+}
+function retrieve_behavior($title) {
+    $con=connect();
+    $query = "SELECT * FROM behaviordb WHERE title='" . $title . "';";
+    $result = mysqli_query($con,$query);
+    
+    if (mysqli_num_rows($result) != 1) {
+        mysqli_close($con);
+        return false;
+    }
+    $result_row = mysqli_fetch_assoc($result);
+    $theBehavior = make_a_behavior($result_row);
+    return $theBehavior;
+}
+function make_a_behavior($result_row) {
+    $theBehavior = new Behavior(
+                   $result_row['title'],
+                   $result_row['behaviorLevel']);
+    return $theBehavior;
 }
 
